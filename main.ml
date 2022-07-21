e53625622d5e544d1a93fca1e164f1f8c02ac495
thedvd98 <>
2022-07-21 19:44:36 +0200

    05

--- main.ml
+++ main.ml
@@ -15,5 +15,22 @@ let rec last_two = function
 let rec at l x = match l with
     | [] -> None
     | hd :: tail when x = 0 -> Some hd
-    | hd :: tail -> at tail (x - 1)
+    | hd :: tail -> at tail (x - 1);;
+
+(* 04 *)
+let rec length l = match l with
+    | [] -> 0
+    | hd :: tail -> 1 + (length tail);;
+(* 04 Tail recursive version *)
+let length_tail l =
+    let rec iter l c = match l with
+        | [] -> c
+        | hd :: tail -> (iter tail (c + 1))
+    in
+        iter l 0;;
+
+(* 05 *)
+let rec rev l = match l with
+    | [] -> []
+    | hd :: tail -> (rev tail) @ [hd];;
 
